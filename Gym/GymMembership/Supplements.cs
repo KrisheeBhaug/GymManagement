414f3b020f787afdf13b7af3d45aac38329ecb7c
Kritisha02 <KS1745@live.mdx.ac.uk>
2025-04-13 00:26:17 +0400

    WIP: before pulling remote changes

--- Gym/GymMembership/Supplements.cs
+++ Gym/GymMembership/Supplements.cs
@@ -16,5 +16,35 @@ namespace GymMembership
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown4_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown5_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
